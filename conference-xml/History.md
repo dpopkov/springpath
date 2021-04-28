5b780608bdeb6d718ee03407016997f1854d4258
Denis Popkov <pkvdenis@gmail.com>
2021-04-28 22:38:10 +0300

    conference-xml: xml Spring configuration (constructor injection).

--- conference-xml/History.md
+++ conference-xml/History.md
@@ -1,8 +1,9 @@
 # History
 
 ### 2 - Add XML configuration
-* Config beans in applicationContext.xml
+* Config beans in applicationContext.xml (setter injection)
 * Use ClassPathXmlApplicationContext to bootstrap application
+* Config beans in applicationContext.xml (constructor injection)
 
 ### 1 - Add Model, Repository and Service for Speaker (without Spring)
 * Speaker
--- conference-xml/src/main/java/learn/springpath/conference/service/SpeakerServiceImpl.java
+++ conference-xml/src/main/java/learn/springpath/conference/service/SpeakerServiceImpl.java
@@ -9,6 +9,13 @@ public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository speakerRepository;
 
+    public SpeakerServiceImpl() {
+    }
+
+    public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+        this.speakerRepository = speakerRepository;
+    }
+
     public void setSpeakerRepository(SpeakerRepository speakerRepository) {
         this.speakerRepository = speakerRepository;
     }
